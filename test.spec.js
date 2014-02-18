c7749d2ec322960949228fd22e9ceb65eff857d1
Stephen Parker <shurakaisoft@gmail.com>
2014-02-18 16:58:50 +1100

    Initial Commit

---
+++ test.spec.js
@@ -0,0 +1,18 @@
+/**
+ * Unit tests
+ *
+ *
+ */
+
+
+var should = require('should');
+
+
+describe("Script", function () {
+    describe("Load Data into Database", function () {
+        it("should connect to a database");
+        it("should have a json object representing data records");
+        it("should add documents to collections");
+    });
+});
+
