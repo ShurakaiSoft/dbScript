d56ac0dbf2a1495dad393d48e297a1f5cf3b4f2e
Stephen Parker <shurakaisoft@gmail.com>
2014-02-18 17:29:06 +1100

    First test

--- test.spec.js
+++ test.spec.js
@@ -4,14 +4,21 @@
  *
  */
 
-
+// Dependencies
 var should = require('should');
 
-
+/**
+ * Unit Tests
+ */
 describe("Script", function () {
+	it("should exist", function () {
+		var app = require('./dbScript');
+		should.exist(app);
+	});
+	describe("Database Connection", function () {
+		it("should have a connection to a database");
+	});
     describe("Load Data into Database", function () {
-        it("should connect to a database");
-        it("should have a json object representing data records");
         it("should add documents to collections");
     });
 });
